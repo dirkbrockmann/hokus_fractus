fc25b8c75efa4afaa4dcb7ac72dc38c57ff8ffeb
Dirk Brockmannn <brockmann.dirk@gmail.com>
2023-12-16 20:27:28 +0100

    changed border on display window

--- src/container_config.js
+++ src/container_config.js
@@ -6,7 +6,7 @@ export default {
 	display_type:"svg", // either svg or canvas depending on explorable
 	debug:false,  // if set to true, draws dots on the control panel to help widget placement
 	controls_border:false,
-	display_border:false,
+	display_border:true,
 	debug_lattice:"debug-grid-16",
 	controls_grid:{nx:12,ny:12},
 	display_size: {width:500,height:500},
